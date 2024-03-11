08ad5690c8c7b00b1ba91907f138878121eb2e55
lana lorena <allana.rocha.delorena@gmail.com>
2024-03-11 14:52:37 -0300

    subindo arquivos

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('fala meu chapa!'));
+
+
 console.log('olá mundo');
+console.log(' A interface File provê informações sobre arquivos e permite ao JavaScript  a acessar seu conteúdo.');
+console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) ');
+
+console.log(chalk.blue.bgWhite.bold("SENAI"));
+
+console.log(chalk.blue('curso','de','node.js'));
+
+console.log(chalk.red('VERMELHO',chalk.underline.bgBlue('azul')));
+
+console.log(`CPU: ${chalk.red('90%')} 
+RAM ${chalk.red('40%')}
+DISK: ${chalk.yellow('70%')}`);
+
+
+
